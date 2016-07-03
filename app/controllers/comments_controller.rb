665d423017978eb85deb02f795141d43b4c0e9af
AndrewZhu <AndrewZhulanov89@gmail.com>
2016-07-03 04:02:38 +0300

    Add Comment MVC

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+	def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new    
+  end
+
+  def create
+    @comment = Comment.create(comment_params)
+    
+    if @comment.save
+      flash[:success] = 'Your comment was successfully added!'
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:title,
+                                      :body,
+                                      :author)
+    end
+end
